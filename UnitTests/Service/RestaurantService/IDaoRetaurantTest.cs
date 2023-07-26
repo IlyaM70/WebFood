d2f07de09c0b269f94bfa0c5c5f9576c1c01cfd9
IlyaM70 <iliya.merkulov@mail.ru>
2023-07-26 11:26:58 +0500

    tests

--- UnitTests/Service/RestaurantService/IDaoRetaurantTest.cs
+++ UnitTests/Service/RestaurantService/IDaoRetaurantTest.cs
@@ -10,7 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WebFood.Service.RestaurantService;
-using WebPlanner.Models;
+
 
 namespace UnitTests.Service.RestaurantService
 {
